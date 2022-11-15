3dd2a698b78e97e03d0c8414acaa6c3b039febba
hfsamrts <harunfazlic@yahoo.com>
2022-11-15 06:27:22 +0100

    TEL-28

--- TellUs/ViewController.swift
+++ TellUs/ViewController.swift
@@ -11,20 +11,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var emailLabel: UITextField!
+    @IBOutlet var passwordLabel: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view
+     
+        emailLabel.attributedPlaceholder = NSAttributedString(
+            string: "Email",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black]
+        )
+        
+        passwordLabel.attributedPlaceholder = NSAttributedString(
+            string: "Password",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.black]
+        )
         
         
         
-        
-        
-        
- 
-    
     }
-
-
+    
+    
 }
 
