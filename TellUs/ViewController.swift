b41d0cd63a79f2e8300fc163ab0b3f1fdfd57e8c
hfsamrts <harunfazlic@yahoo.com>
2022-11-25 21:50:04 +0100

    TEL-58

--- TellUs/ViewController.swift
+++ TellUs/ViewController.swift
@@ -9,15 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    /*TEL62*/
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var signInButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         emailTextField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black]) /*TEL-5*/
         passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.black])/*TEL-6*/
@@ -60,7 +59,11 @@ class ViewController: UIViewController {
     
     
     @IBAction func aboutButton(_ sender: UIButton) { /*TEL-59*/
-        print("aboutButton is clicked ")
+        /*TEL-58*/
+        let alert = UIAlertController(title: "HFSS", message: "copyright © 2022 all rights reserved", preferredStyle: .alert)
+        let doneWithAlert = UIAlertAction(title: "Ok", style: .default)
+        alert.addAction(doneWithAlert)
+        present(alert, animated: true, completion: nil)
     }
     
     
