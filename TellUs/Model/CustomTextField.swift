914e53517fe5b8f0970c2d3694c069c7de5c6f9b
hfsamrts <harunfazlic@yahoo.com>
2022-11-30 19:49:47 +0100

    TEL-92

--- TellUs/Model/CustomTextField.swift
+++ TellUs/Model/CustomTextField.swift
@@ -23,7 +23,7 @@ class CustomTextField: UITextField{
     /*TEL-91*/
     func setUpTextField(){
         self.textColor = .black
-        self.backgroundColor = .white //D9D9D9
+        self.backgroundColor = UIColor(rgb: 0xD9D9D9)
         self.borderStyle = .roundedRect
         self.font = UIFont(name: "KleeOne-SemiBold", size: 18)
         self.delegate = self
@@ -40,4 +40,23 @@ extension CustomTextField: UITextFieldDelegate{ /*TEL-44*/ /*TEL-32*/ /*TEL-33*/
         textField.resignFirstResponder()
         return true
     }
+}
+
+/*TEL-92*/
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
 }
