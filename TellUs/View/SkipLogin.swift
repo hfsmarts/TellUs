a06091bc2580e8fce747d8fdb2979d48d8486477
hfsamrts <harunfazlic@yahoo.com>
2022-12-04 10:10:46 +0100

    TEL-107

---
+++ TellUs/View/SkipLogin.swift
@@ -0,0 +1,29 @@
+//
+//  SkipLogin.swift
+//  TellUs
+//
+//  Created by Harun Fazlic on 4. 12. 2022..
+//
+
+import UIKit
+
+class SkipLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
