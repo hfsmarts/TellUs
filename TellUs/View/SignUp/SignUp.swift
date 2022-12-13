19c7b5695bb10b513434523ff2dd6c44c3cd7231
hfsamrts <harunfazlic@yahoo.com>
2022-12-13 06:10:42 +0100

    TEL-124

--- TellUs/View/SignUp/SignUp.swift
+++ TellUs/View/SignUp/SignUp.swift
@@ -13,19 +13,10 @@ class SignUp: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 0.99, green: 0.50, blue: 0.14, alpha: 1) /*TEL-124*/
 
-        // Do any additional setup after loading the view.
+        
+
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
