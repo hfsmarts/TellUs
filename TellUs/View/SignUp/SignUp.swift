465d69688b9dc3435552d39156b2d647b40fe3c0
hfsamrts <harunfazlic@yahoo.com>
2022-12-13 05:55:11 +0100

    TEL-121

---
+++ TellUs/View/SignUp/SignUp.swift
@@ -0,0 +1,31 @@
+//
+//  SignUp.swift
+//  TellUs
+//
+//  Created by Harun Fazlic on 13. 12. 2022..
+//
+
+import UIKit
+
+/*TEL-121*/
+
+class SignUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
