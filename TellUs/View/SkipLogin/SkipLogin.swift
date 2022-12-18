f75a20f01014b5554893ab2bfb660b4d706cec1f
hfsamrts <harunfazlic@yahoo.com>
2022-12-18 09:25:01 +0100

    TEL-141

--- TellUs/View/SkipLogin/SkipLogin.swift
+++ TellUs/View/SkipLogin/SkipLogin.swift
@@ -9,22 +9,13 @@ import UIKit
 
 class SkipLogin: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.15, green: 0.53, blue: 0.90, alpha: 1.00) /*TEL-140*/
 
 
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
