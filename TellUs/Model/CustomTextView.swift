cb3d4387d5986914313d83378064f3962d072489
hfsamrts <harunfazlic@yahoo.com>
2022-12-19 07:54:15 +0100

    TEL-147

---
+++ TellUs/Model/CustomTextView.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTextView.swift
+//  TellUs
+//
+//  Created by Harun Fazlic on 19. 12. 2022..
+//
+
+import UIKit
+
+class CustomTextView: UITextView{
+    
+    
+    
+    
+    
+}
