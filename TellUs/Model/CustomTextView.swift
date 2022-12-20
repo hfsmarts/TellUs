5ea7c1f323058ad1921712a8892821800bf0b565
hfsamrts <harunfazlic@yahoo.com>
2022-12-20 07:02:22 +0100

    TEL-167

--- TellUs/Model/CustomTextView.swift
+++ TellUs/Model/CustomTextView.swift
@@ -25,10 +25,9 @@ class CustomTextView: UITextView{
         self.autocorrectionType = UITextAutocorrectionType.no /*TEL-151*/
         self.spellCheckingType = UITextSpellCheckingType.no   /*TEL-151*/
         self.autocapitalizationType = UITextAutocapitalizationType.none /*TEL-151*/
+        self.isEditable = false /*TEL-167*/
+        self.isSelectable = false /*TEL-167*/
               
-      
-      
-        
     }
 }
 
