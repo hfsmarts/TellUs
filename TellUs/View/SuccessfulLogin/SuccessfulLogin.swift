3d7769412e88dfd30d47be858ffe21db009860fa
hfsamrts <harunfazlic@yahoo.com>
2022-12-23 07:21:52 +0100

    TEL-227

--- TellUs/View/SuccessfulLogin/SuccessfulLogin.swift
+++ TellUs/View/SuccessfulLogin/SuccessfulLogin.swift
@@ -10,10 +10,12 @@ import UIKit
 /*TEL-172*/
 class SuccessfulLogin: UIViewController {
 
+    @IBOutlet var successfulLoginPostText: CustomTextView! /*TEL-227*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.15, green: 0.53, blue: 0.90, alpha: 1.0) /*TEL-173*/
 
-        
+        successfulLoginPostText.text = "Idu dva balona ulicom i sretnu kaktussssssssssssssssssss"
     }
 }
