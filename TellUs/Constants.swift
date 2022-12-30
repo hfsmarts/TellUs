9e7fc83c722ffed810f28f1695cfc6d0c1ef0e5f
hfsamrts <harunfazlic@yahoo.com>
2022-12-30 05:53:25 +0100

    TEL-256

--- TellUs/Constants.swift
+++ TellUs/Constants.swift
@@ -10,4 +10,11 @@ import UIKit
 /*TEL-221*/
 class K {
     let message = "Something went wrong. \n Password must be at least 8 character long. Password should contain capital case letters and numbers." /*TEL-222*/
+    
+    
+    
+    
+    
+    let appRedColor = UIColor(named: "AppRed") /*TEL-256*/
+
 }
