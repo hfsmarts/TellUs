b6b1c8779472ee01d61f0dfd966c40e3d5b8021b
hfsamrts <harunfazlic@yahoo.com>
2023-01-04 07:43:15 +0100

    TEL-263

--- TellUs/Constants.swift
+++ TellUs/Constants.swift
@@ -9,15 +9,15 @@ import UIKit
 
 /*TEL-221*/
 struct K { /*TEL-259*/ /*TEL-222*/
-   static let signUpErrorMessage = "Something went wrong. \n Password must be at least 8 character long. Password should contain capital case letters and numbers."
+    static let signUpErrorMessage = "Something went wrong. \n Password must be at least 8 character long. Password should contain capital case letters and numbers."
     
     
     /*TEL-258*/
     struct AppColors{
-        //Issue color is not loading
-       static let red = "AppRed"
+        static let red = "AppRed" /*TEL-256*/
+        static let green = "AppGreen" /*TEL-263*/
     }
     
     
-
+    
 }
