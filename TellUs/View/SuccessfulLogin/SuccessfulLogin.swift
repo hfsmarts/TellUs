244b81cfc36104dbdb424c17e2bd198b672735a4
hfsamrts <harunfazlic@yahoo.com>
2023-01-04 11:42:32 +0100

    TEL-268

--- TellUs/View/SuccessfulLogin/SuccessfulLogin.swift
+++ TellUs/View/SuccessfulLogin/SuccessfulLogin.swift
@@ -9,16 +9,15 @@ import UIKit
 
 /*TEL-172*/
 class SuccessfulLogin: UIViewController {
-
+    
     @IBOutlet var successfulLoginPostText: CustomTextView! /*TEL-227*/
     @IBOutlet var dislikeCounter: UILabel! /*TEL-255*/
     @IBOutlet var likeCounter: UILabel! /*TEL-265*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundBlue") /*TEL-173*/ /*TEL-268*/
         
-        view.backgroundColor = UIColor(red: 0.15, green: 0.53, blue: 0.90, alpha: 1.0) /*TEL-173*/
-                
         dislikeCounter.textColor = UIColor(named: K.AppColors.red) /*TEL-257*/
         likeCounter.textColor = UIColor(named: K.AppColors.green) /*TEL-264*/
         
@@ -32,7 +31,7 @@ class SuccessfulLogin: UIViewController {
     }
     
     
-  /*TEL-237*/
+    /*TEL-237*/
     @IBAction func backButton(_ sender: UIButton) { /*TEL-239*/
         dismiss(animated: true, completion: nil) /*TEL-240*/
     }
