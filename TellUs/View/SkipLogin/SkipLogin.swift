2120aaeab1216f8fd8ff10d72c4426722fb6bf8a
hfsamrts <harunfazlic@yahoo.com>
2023-01-05 07:39:11 +0100

    TEL-279

--- TellUs/View/SkipLogin/SkipLogin.swift
+++ TellUs/View/SkipLogin/SkipLogin.swift
@@ -10,10 +10,12 @@ import UIKit
 class SkipLogin: UIViewController {
     
     @IBOutlet var skipLoginPostText: CustomTextView!
+    @IBOutlet var backButton: CustomButton! /*TEL-279*/
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: K.AppColors.blueBackground) /*TEL-271*/ /*TEL-140*/
+        
     }
     
     override func viewDidLayoutSubviews() {
