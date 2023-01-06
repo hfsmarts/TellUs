b55dd7215ddeac278cd4ed7522b12715f2963e2c
hfsamrts <harunfazlic@yahoo.com>
2023-01-06 07:53:30 +0100

    TEL-278

--- TellUs/Model/BackButton.swift
+++ TellUs/Model/BackButton.swift
@@ -5,8 +5,23 @@
 //  Created by Harun Fazlic on 6. 1. 2023..
 //
 
-/*TEL-290*/
+import UIKit
 
-import Foundation
+/*TEL-290*/ /*TEL-278*/
 
+class BackButton: UIButton {
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUPButton()
+    }
+    
+    func setUPButton(){
+        setTitleColor(.black, for: .normal)
+        backgroundColor = .clear /*TEL-284*/
+        titleLabel?.font = UIFont(name: "KleeOne-SemiBold", size: 25) /*TEL-286*/
+        setTitle("back", for: .normal) /*TEL-285*/
+    }
+    
+    
+}
