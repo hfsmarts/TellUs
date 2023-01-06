10db0598a4bc03ef730baca76edaadd6367df71b
hfsamrts <harunfazlic@yahoo.com>
2023-01-06 17:02:26 +0100

    TEL-313

---
+++ TellUs/View/MyPosts/MyPosts.swift
@@ -0,0 +1,29 @@
+//
+//  MyPosts.swift
+//  TellUs
+//
+//  Created by Harun Fazlic on 6. 1. 2023..
+//
+
+import UIKit
+
+class MyPosts: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
