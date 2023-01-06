afffab70e4740d862c052a9175b6c242ede90cc7
hfsamrts <harunfazlic@yahoo.com>
2023-01-06 15:44:36 +0100

    TEL-303

--- TellUs/View/UserNavigation/UserNavigation.swift
+++ TellUs/View/UserNavigation/UserNavigation.swift
@@ -9,10 +9,12 @@ import UIKit
 
 /*TEL-296*/
 class UserNavigation: UIViewController {
+    @IBOutlet var newPostButton: CustomButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "BackgroundOrange") /*TEL-297*/
+        newPostButton.backgroundColor = UIColor(named: "BackgroundBlue")
     }
     
     
