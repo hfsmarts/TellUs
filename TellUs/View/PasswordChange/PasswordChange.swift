6f9711e064b2c9a5bd6e6483ddd7fccfb6f9b882
hfsamrts <harunfazlic@yahoo.com>
2023-01-09 06:11:33 +0100

    TEL-353

--- TellUs/View/PasswordChange/PasswordChange.swift
+++ TellUs/View/PasswordChange/PasswordChange.swift
@@ -12,19 +12,10 @@ class PasswordChange: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(named: K.AppColors.orangeBackground) /*TEL-353*/
 
-        // Do any additional setup after loading the view.
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
