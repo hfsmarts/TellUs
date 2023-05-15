6ff9c1a0791a46211c1340335c60ec291d549463
hfsmarts <harunfazlic@yahoo.com>
2023-05-15 06:26:11 +0200

    TEL-623

--- TellUs/View/SuccessfulLogin/SuccessfulLogin.swift
+++ TellUs/View/SuccessfulLogin/SuccessfulLogin.swift
@@ -9,9 +9,10 @@ import UIKit
 
 /*TEL-172*/
 class SuccessfulLogin: UIViewController, UICollectionViewDataSource {
-    
+   
     @IBOutlet var successCollectionView: SuccessCollectionView!
-    
+    var objects: [PostModel] = [] /*TEL-623*/
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,17 +22,40 @@ class SuccessfulLogin: UIViewController, UICollectionViewDataSource {
         
         //Text below should be loaded from database, or should be begining info for the user, how to use the app
         //successfulLoginPostText.text = Constants.successfulLoginPostText /*TEL-435*/
+        
+        let object1 = PostModel(location: "Location 1", likeCount: 10, text: "Text 1", dislikeCount: 5)
+        let object2 = PostModel(location: "Location 2", likeCount: 5, text: "Text 2", dislikeCount: 2)
+        let object3 = PostModel(location: "Location 3", likeCount: 15, text: "Text 3", dislikeCount: 8)
+        let object4 = PostModel(location: "Location 4", likeCount: 3, text: "Text 4", dislikeCount: 1)
+        let object5 = PostModel(location: "Location 5", likeCount: 7, text: "Text 5", dislikeCount: 4)
+        
+        objects.append(object1)
+        objects.append(object2)
+        objects.append(object3)
+        objects.append(object4)
+        objects.append(object5)
+        
     }
-    
+        
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        20
+        return objects.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CollectionViewCell.successCellIdentifier, for: indexPath) as! SuccessCollectionViewCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.CollectionViewCell.successCellIdentifier, for: indexPath) as? SuccessCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let object = objects[indexPath.row]
+        cell.locationLabel.text = object.location
+        cell.likeCounterLabel.text = "\(object.likeCount)"
+        cell.textViewText.text = object.text
+        cell.dislikeCounterLabel.text = "\(object.dislikeCount)"
+
         return cell
     }
     
+
     /*TEL-438*/
     @IBAction func hamburgerButton(_ sender: UIButton) { /*TEL-455*/
         let userNavigationVC = UserNavigation() /*TEL-456*/
